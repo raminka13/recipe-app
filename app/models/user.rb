803e22552ceb45338a48d4191b52eeae5e3c28a5
Raul Ospina <raminka13@hotmail.com>
2022-11-07 14:49:51 -0500

    Generate User views, controller, model and routes

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_many :recipes, inverse_of: 'user', foreign_key: 'user_id', dependent: :destroy
+  has_many :foods, inverse_of: 'user', foreign_key: 'user_id', dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 27 }
+end
