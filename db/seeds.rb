e3d9f645bd4feaa238f5b852c75f95b838d98e34
Raul Ospina <raminka13@hotmail.com>
2022-11-07 19:41:36 -0500

    Add DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+9.times do |user|
+  User.create(name: Faker::Name.name)
+end
+
+9.times do
+  Food.create(name: Faker::Food.fruits, measurement_unit: Faker::Food.metric_measurement, price: rand(1..27), quantity: rand(1..9), user_id: rand(1..9))
+  Food.create(name: Faker::Food.ingredient, measurement_unit: Faker::Food.metric_measurement, price: rand(1..27), quantity: rand(1..9), user_id: rand(1..9))
+  Food.create(name: Faker::Food.vegetables, measurement_unit: Faker::Food.metric_measurement, price: rand(1..27), quantity: rand(1..9), user_id: rand(1..9))
+end
