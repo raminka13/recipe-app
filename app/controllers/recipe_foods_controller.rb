fb1b6bd4119c76dac2f60d3ced07fa33cda40310
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-11-08 19:48:46 -0300

    Fix linters

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,7 +1,7 @@
 class RecipeFoodsController < ApplicationController
   before_action :obtain_user
   before_action :obtain_recipe
-  before_action :set_recipe_food, only: %i[ show edit update destroy ]
+  before_action :set_recipe_food, only: %i[show edit update destroy]
 
   # GET /recipe_foods or /recipe_foods.json
   def index
@@ -9,8 +9,7 @@ class RecipeFoodsController < ApplicationController
   end
 
   # GET /recipe_foods/1 or /recipe_foods/1.json
-  def show
-  end
+  def show; end
 
   # GET /recipe_foods/new
   def new
@@ -18,8 +17,7 @@ class RecipeFoodsController < ApplicationController
   end
 
   # GET /recipe_foods/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /recipe_foods or /recipe_foods.json
   def create
@@ -27,7 +25,7 @@ class RecipeFoodsController < ApplicationController
 
     respond_to do |format|
       if @recipe_food.save
-        format.html { redirect_to user_recipe_url(@user, @recipe), notice: "Recipe food was successfully created." }
+        format.html { redirect_to user_recipe_url(@user, @recipe), notice: 'Recipe food was successfully created.' }
         format.json { render :show, status: :created, location: @recipe_food }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -40,7 +38,7 @@ class RecipeFoodsController < ApplicationController
   def update
     respond_to do |format|
       if @recipe_food.update(recipe_food_params)
-        format.html { redirect_to user_recipe_url(@user, @recipe), notice: "Recipe food was successfully updated." }
+        format.html { redirect_to user_recipe_url(@user, @recipe), notice: 'Recipe food was successfully updated.' }
         format.json { render :show, status: :ok, location: @recipe_food }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -54,27 +52,28 @@ class RecipeFoodsController < ApplicationController
     @recipe_food.destroy
 
     respond_to do |format|
-      format.html { redirect_to user_recipe_path(@user, @recipe), notice: "Recipe food was successfully destroyed." }
+      format.html { redirect_to user_recipe_path(@user, @recipe), notice: 'Recipe food was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
 
-    def obtain_user
-      @user = User.find(params[:user_id])
-    end
+  def obtain_user
+    @user = User.find(params[:user_id])
+  end
 
-    def obtain_recipe
-      @recipe = Recipe.find(params[:recipe_id])
-    end
-    # Use callbacks to share common setup or constraints between actions.
-    def set_recipe_food
-      @recipe_food = RecipeFood.find(params[:id])
-    end
+  def obtain_recipe
+    @recipe = Recipe.find(params[:recipe_id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def recipe_food_params
-      params.require(:recipe_food).permit(:quantity, :recipe_id, :food_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_recipe_food
+    @recipe_food = RecipeFood.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def recipe_food_params
+    params.require(:recipe_food).permit(:quantity, :recipe_id, :food_id)
+  end
 end
