e611360c5f7ec883338b407dfb04152f3a6f8163
Raul Ospina <raminka13@hotmail.com>
2022-11-08 14:08:10 -0500

    Generate Recipe Foods

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
+  has_many :recipe_foods, inverse_of: 'food', foreign_key: 'food_id', dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 18 }
   validates :measurement_unit, presence: true, length: { maximum: 9 }
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :recipe_foods, inverse_of: 'recipe', foreign_key: 'recipe_id', dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 18 }
   validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
