1f7b9a681708bd37253b1ae99ac6682bee8cf86b
Raul Ospina <raminka13@hotmail.com>
2022-11-09 11:30:06 -0500

    Add toggle button for public recipe

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @public_recipes = Recipe.where(public: true).order('id DESC')
+    @public_recipes = Recipe.all.where(public: true).order('id DESC')
   end
 end
