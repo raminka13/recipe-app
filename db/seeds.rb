334d243e2ff3fd9c0a2f93c8735150839e1478a3
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-11-09 17:57:15 -0300

    Add registration and login page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-81.times do
-  RecipeFood.create(quantity: rand(1..18), recipe_id: rand(1..9), food_id: rand(1..9))
-end
