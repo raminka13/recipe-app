9d94c860e9e7a5f1467c2f6550e6117b3cfde17b
Raul Ospina <raminka13@hotmail.com>
2022-11-09 16:38:50 -0500

    Add Navbar and some styles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,7 +59,7 @@ class UsersController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_user
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
   # Only allow a list of trusted parameters through.
