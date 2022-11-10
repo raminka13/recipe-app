094c1a585c4671528b30e8a9c99ee3d57aaea4b9
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-11-10 13:52:27 -0300

    Add unit testings for Recipe model

---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :model do
+  subject { User.new(name: 'Pepe Frog', email: 'test@example.com', password: 'password', password_confirmation: 'password') }
+  before { subject.save }
+  before { @recipe = Recipe.create(name: 'apple pie', preparation_time: 10, cooking_time: 10, description: 'Tasty!', public: true, user: subject) }
+
+  context 'Validations should be working' do
+    it 'ALL validations should return true' do
+      expect(subject).to be_valid
+    end
+
+    it 'ALL validations should return true' do
+      expect(@recipe.name).to eql('apple pie')
+    end
+
+    it 'ALL validations should return true' do
+      expect(@recipe.preparation_time).to be_a_kind_of(Numeric)
+    end
+
+    it 'ALL validations should return true' do
+      expect(@recipe.cooking_time).to be(10)
+    end
+
+    it 'ALL validations should return true' do
+      expect(@recipe.public).to be(true)
+    end
+  end
+end
