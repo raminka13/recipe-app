0da149181b919963f3d2da30d95470ab3b53b2ef
Raul Ospina <raminka13@hotmail.com>
2022-11-09 19:44:12 -0500

    N+1 Optimizations

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @public_recipes = Recipe.all.where(public: true).order('id DESC')
+    @public_recipes = Recipe.all.includes([:user]).where(public: true).order('id DESC')
   end
 end
