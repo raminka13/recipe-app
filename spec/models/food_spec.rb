3250d962130fd503daff82a1113407ddc053cca9
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-11-10 13:44:47 -0300

    Add unit testings for Food model

---
+++ spec/models/food_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Food, type: :model do
+  subject { User.new(name: 'Pepe Frog', email: 'test@example.com', password: 'password', password_confirmation: 'password') }
+  before { subject.save }
+  before { @apple = Food.create(name: 'apple', measurement_unit: 'kg', price: 10, quantity: 10, user: subject) }
+
+  context 'Validations should be working' do
+    it 'ALL validations should return true' do
+      expect(subject).to be_valid
+    end
+
+    it 'ALL validations should return true' do
+      expect(@apple.name).to eql('apple')
+    end
+
+    it 'ALL validations should return true' do
+      expect(@apple.price).to be_a_kind_of(Numeric)
+    end
+
+    it 'ALL validations should return true' do
+      expect(@apple.quantity).to be(10)
+    end
+
+    it 'ALL validations should return true' do
+      expect(@apple.measurement_unit).to eql('kg')
+    end
+  end
+end
